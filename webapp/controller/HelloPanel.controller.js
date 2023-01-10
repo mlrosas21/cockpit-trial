79adf17f68eb0f28942fe23050097d1222200b0d
Martin Luis Rosas Ortmann <martinrosasortmann@gmail.com>
2023-01-10 14:36:41 +0000

    Invocies + fix

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,16 +1,20 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 ], function(
-	Controller
+	Controller,
+    MessageToast,
+    Fragment
 ) {
 	"use strict";
 
 	return Controller.extend("sandbox.controller.HelloPanel", {
         onShowHello : function () {
 			// read msg from i18n model
-			var oBundle = this.getView().getModel("i18n").getResourceBundle();
-			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+			let oBundle = this.getView().getModel("i18n").getResourceBundle();
+			let sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			let sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
 			// show message
 			MessageToast.show(sMsg);
@@ -19,8 +23,10 @@ sap.ui.define([
 		onOpenDialog : function () {
 			// create dialog lazily
 			if (!this.pDialog) {
-				this.pDialog = this.loadFragment({
-					name: "sap.ui.demo.walkthrough.view.HelloDialog"
+				this.pDialog = Fragment.load({
+					name: "sandbox.view.fragments.HelloDialog",
+                    controller: this,
+                    id: this.getView().getId()
 				});
 			}
 
